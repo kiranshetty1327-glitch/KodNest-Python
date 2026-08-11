84b4ee32e08ba7c2af9dd9bbdef598182586f7ae
KIRAN S <kiran825142@gmail.com>
2026-08-11 20:52:10 +0530

    day6

---
+++ module 2/T01/src/multiples of three.py
@@ -0,0 +1,18 @@
+limit = int(input)
+target = int(input)
+
+count = 0
+total = 0
+found = False
+
+for i in range(1,limit+1):
+    if i%3==0:
+        count+=1
+        total+=i
+        if i==target:
+            found = True
+print("Count:",count)
+print("Sum:",total)
+if found:
+    print("Target Found: Yes")
+    print("Target Found: NO")            

721e461123333b719fae4dd3db2e386d61b55f90
KIRAN S <kiran825142@gmail.com>
2026-08-11 21:55:19 +0530

    day11

---
+++ module 2/T01/src/tuple without modfiying.py
@@ -0,0 +1,11 @@
+cname = input()
+cweek = input()
+cstatus = input()
+
+course_details=(cname,cweek,cstatus)
+
+updated_week=input()
+
+course_details=(cname,updated_week,cstatus)
+
+print(course_details)
